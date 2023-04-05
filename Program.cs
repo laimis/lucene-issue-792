6b4596c8c880a4fcfada332c9cc801a81185eaf0
unknown <laimis@gmail.com>
2023-04-05 11:18:42 -0700

    test issue with lucene

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.QueryParsers.Classic;
+using Lucene.Net.Util;
+
+
+Console.OutputEncoding=Encoding.UTF8;
+var std= new StandardAnalyzer(LuceneVersion.LUCENE_48, StandardAnalyzer.STOP_WORDS_SET);
+Console.Write("A");
+var qp = new MultiFieldQueryParser(LuceneVersion.LUCENE_48, new[] {"test"}, std);
+Console.Write("B");
